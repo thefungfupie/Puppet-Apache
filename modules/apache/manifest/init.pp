199a259e846349d526868cc77ffd9d97a40b40fe
unika <thefungfupie@gmail.com>
2014-06-13 11:21:02 -0500

    install apache

--- modules/apache/manifest/init.pp
+++ modules/apache/manifest/init.pp
@@ -1,3 +1,15 @@
 class apache{
-	
+	package {'apache':
+		ensure    => installed,
+	}
+	service { 'apache':
+		ensure    => running,
+		require   => Package['apache'],
+		subscribe => File['/etc/httpd/conf/httpd.conf']
+	}
+	file { '/etc/httpd/conf/httpd.conf':
+		require   => Package['apache'],
+		notify    => Service['apahce'],
+	}
+
 }
