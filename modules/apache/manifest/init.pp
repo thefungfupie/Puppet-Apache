2cdfa535c9903db3e2cf20d61a75345f044289b5
unika <thefungfupie@gmail.com>
2014-06-16 14:42:28 -0500

    template

--- modules/apache/manifest/init.pp
+++ modules/apache/manifest/init.pp
@@ -5,9 +5,10 @@ class apache{
 	service { 'apache':
 		ensure    => running,
 		require   => Package['apache'],
-		subscribe => File['/etc/httpd/conf/httpd.conf']
+		subscribe => File['/etc/httpd/conf/httpd.conf'],
 	}
 	file { '/etc/httpd/conf/httpd.conf':
+		ensure    => file,
 		require   => Package['apache'],
 		notify    => Service['apahce'],
 	}
